b650140d2fa92b18880f0a5eccffa707da467887
Saurabh <purohit10saurabh@gmail.com>
2026-03-07 15:08:24 +0530

    remove warnings

--- index.md
+++ index.md
@@ -6,6 +6,6 @@ permalink: /
 
 Hi, I'm **Saurabh Purohit**.
 
-I like to build complex systems. I write about technical ideas, math, and life.
+I write about machine learning, math, and life on this blog.
 
 Browse my [technical blog](/blog) or read about [life](/life).
---
+++ serve.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~/Documents/website
+eval "$(conda shell.bash hook)"
+conda activate website
+trap "echo -e '\nStopping...'; lsof -ti:4000 | xargs kill -9 2>/dev/null" EXIT
+open http://localhost:4000 & jekyll serve --port 4000 --livereload --force_polling --incremental
