150fc43408929abf9ccd64ef09cf138298819e9e
Saurabh <purohit10saurabh@gmail.com>
2026-04-26 23:30:41 +0530

    Updated website

--- serve.sh
+++ serve.sh
@@ -2,5 +2,15 @@
 cd ~/Documents/website
 eval "$(conda shell.bash hook)"
 conda activate website
+
+if [ -z "$1" ]; then
+    echo "Page name is not provided. Using default index.html"
+    PAGE_NAME=""
+else
+    PAGE_NAME="$1/"
+fi
+
+echo "Serving page $PAGE_NAME"
+
 trap "echo -e '\nStopping...'; lsof -ti:4000 | xargs kill -9 2>/dev/null" EXIT
-open http://localhost:4000 & jekyll serve --port 4000 --livereload --force_polling
+open http://localhost:4000/$PAGE_NAME & jekyll serve --port 4000 --livereload --force_polling
